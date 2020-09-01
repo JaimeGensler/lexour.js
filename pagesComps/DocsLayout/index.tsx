52029e92b0559dd2c2db6ae959d381cdce700a05
Jaime Gensler <jaimegensler0@gmail.com>
2020-09-01 10:00:08 -0700

    Update Docs Layout

--- pagesComps/DocsLayout/index.tsx
+++ pagesComps/DocsLayout/index.tsx
@@ -1,24 +1,22 @@
-import Head from 'next/head';
 import Nav from './Nav';
 import Header from './Header';
 
-export default function withDocsLayout(Page: () => JSX.Element, title: string) {
-    return () => {
-        return (
-            <>
-                <Head>
-                    <title>{title}</title>
-                </Head>
-                <div className="w-full h-screen">
-                    <Header />
-                    <div className="flex h-full pt-16">
-                        <Nav />
-                        <main className="flex-1 text-lg px-12 py-8 h-full overflow-y-scroll">
-                            <Page />
-                        </main>
-                    </div>
-                </div>
-            </>
-        );
-    };
+type Props = {
+    children: JSX.Element;
+    title: string;
+};
+
+export default function DocsLayout({ children, title }: Props) {
+    return (
+        <div className="w-full h-screen">
+            <Header />
+            <div className="flex h-full pt-16">
+                <Nav />
+                <main className="flex-1 text-lg px-12 py-8 h-full overflow-y-auto">
+                    <h1 className="font-bold text-3xl">{title}</h1>
+                    {children}
+                </main>
+            </div>
+        </div>
+    );
 }
