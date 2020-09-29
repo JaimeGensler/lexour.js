6fac41f9594c0a0d94f96d2c60a7783bad008f9f
Jaime Gensler <jaimegensler0@gmail.com>
2020-09-29 13:01:24 -0700

    Remove old files

--- src/utils/getSpacedLineNumber.ts
+++
@@ -1,4 +0,0 @@
-export default function getSpacedLineNumber(current: number, largest: number) {
-    const ln = String(current);
-    return `${' '.repeat(String(largest).length - ln.length)}${ln}`;
-}
