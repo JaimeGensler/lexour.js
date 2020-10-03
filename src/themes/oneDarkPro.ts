f63ec702d2fd269199f370b0f4713b9529ca4d6f
Jaime Gensler <jaimegensler0@gmail.com>
2020-10-03 13:19:58 -0700

    Add string.interpolation to ODP theming

--- src/themes/oneDarkPro.ts
+++ src/themes/oneDarkPro.ts
@@ -9,7 +9,7 @@ export default buildTheme({ color: '#abb2bf', background: '#282c34' })
         textDecorationLine: 'underline',
         textDecorationColor: '#f44747',
     })
-    .color('#c678dd', ['keyword'])
+    .color('#c678dd', ['keyword', 'string.interpolation'])
     .color('#e06c75', ['variable', 'tag.name', 'text.escape'])
     .color('#d19a66', ['literal', 'tag.attribute'])
     .color('#e5c07b', ['constant'])
