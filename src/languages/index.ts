c71201798dbb1d4fb5a76832486309b98b17abb1
Jaime Gensler <jaimegensler0@gmail.com>
2020-10-03 13:22:09 -0700

    Add strict typing of languages object, add JS

--- src/languages/index.ts
+++ src/languages/index.ts
@@ -1,11 +1,18 @@
+import type { BuiltInLang, Lexer } from '../types';
+
 import html from './html';
 import terminal from './terminal';
 import json from './json';
 import css from './css';
+import { js } from './javascript';
 
-export default {
+const languages: Record<BuiltInLang, Lexer> = {
     html,
     terminal,
     json,
     css,
+    javascript: js,
+    js,
 };
+
+export default languages;
