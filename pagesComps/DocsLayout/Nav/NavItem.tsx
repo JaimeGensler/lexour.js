c284309584d537d50f973ea448ff2aef0881390e
Jaime Gensler <jaimegensler0@gmail.com>
2020-10-09 15:14:21 -0700

    Change conditional styles to use clsx

--- pagesComps/DocsLayout/Nav/NavItem.tsx
+++ pagesComps/DocsLayout/Nav/NavItem.tsx
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/router';
+import clsx from 'clsx';
 import NavSubItem from './NavSubItem';
 import Link from 'next/link';
 import Conditional from '../../utils/Conditional';
@@ -15,25 +16,29 @@ type Props = {
 
 export default function NavItem({ title, pageURL, subItems }: Props) {
     const { pathname } = useRouter();
-    const isCurrentPage = pathname === pageURL;
+    const isCurrentPage = useRouter() === pageURL;
 
     const sublist = isCurrentPage
         ? subItems.map(item => <NavSubItem {...item} key={item.title} />)
         : null;
 
-    const listStyle = isCurrentPage
-        ? 'border-t border-b bg-gray-200 py-2'
-        : 'border-r py-1';
-
-    const boldStyle = isCurrentPage
-        ? 'font-bold'
-        : 'font-medium hover:font-bold focus:font-bold';
-
     return (
-        <li className={`${listStyle} px-8`}>
+        <li
+            className={clsx(
+                'px-8',
+                isCurrentPage
+                    ? 'border-t border-b bg-gray-200 py-2'
+                    : 'border-r py-1',
+            )}
+        >
             <Link href={isCurrentPage ? '#content-top' : pageURL}>
                 <a
-                    className={`group transition-all duration-200 block text-xl ${boldStyle}`}
+                    className={clsx(
+                        'group transition-all duration-200 block text-xl',
+                        isCurrentPage
+                            ? 'font-bold text-blue-600'
+                            : 'font-medium hover:font-bold focus:font-bold',
+                    )}
                 >
                     <span className="inline-block transform group-hover:translate-x-1 group-focus:translate-x-1 transition-transform duration-200">
                         {title}
